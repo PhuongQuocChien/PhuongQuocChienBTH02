36add4537b0bae0933ea0d57c877976f833d917d
PhuongQuocChien <chienquocphuong@gmail.com>
2022-11-23 07:54:06 +0700

    PQC_16/11

---
+++ BTHChon/Migrations/20221122054913_Create_Table_Faculty.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTHChon.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableFaculty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Faculty",
+                columns: table => new
+                {
+                    FacultyID = table.Column<string>(type: "TEXT", nullable: false),
+                    FacultyName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculty", x => x.FacultyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    StudentID = table.Column<string>(type: "TEXT", nullable: false),
+                    StudentName = table.Column<string>(type: "TEXT", nullable: true),
+                    FacultyID = table.Column<string>(type: "TEXT", nullable: true),
+                    StudentAddress = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.StudentID);
+                    table.ForeignKey(
+                        name: "FK_Student_Faculty_FacultyID",
+                        column: x => x.FacultyID,
+                        principalTable: "Faculty",
+                        principalColumn: "FacultyID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_FacultyID",
+                table: "Student",
+                column: "FacultyID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Faculty");
+        }
+    }
+}
---
+++ BTHChon/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BTHChon.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
